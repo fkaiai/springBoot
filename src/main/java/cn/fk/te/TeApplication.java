b4265dafd7c535aca6a285f939514a05b4730b58
kai.feng <12345678>
2018-03-28 10:47:14 +0800

    init

---
+++ src/main/java/cn/fk/te/TeApplication.java
@@ -0,0 +1,12 @@
+package cn.fk.te;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeApplication.class, args);
+	}
+}
