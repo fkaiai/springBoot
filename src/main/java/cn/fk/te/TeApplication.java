8f7b60500c797263f7b8e6d26ab0bc450416f847
kai.feng <12345678>
2018-03-29 10:25:33 +0800

    mybatis generator自动生成实体、mapper、xml测试

--- src/main/java/cn/fk/te/TeApplication.java
+++ src/main/java/cn/fk/te/TeApplication.java
@@ -1,9 +1,13 @@
 package cn.fk.te;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@MapperScan("cn.fk.te.mapper")
 public class TeApplication {
 
 	public static void main(String[] args) {
--- src/test/java/cn/fk/te/TeApplicationTests.java
+++ src/test/java/cn/fk/te/TeApplicationTests.java
@@ -1,7 +1,9 @@
 package cn.fk.te;
 
+import cn.fk.te.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TeApplicationTests {
 
+	@Autowired
+	UserMapper userMapper;
+
 	@Test
 	public void contextLoads() {
+		System.out.println(userMapper.selectByPrimaryKey(16).toString());
 	}
 
 }
