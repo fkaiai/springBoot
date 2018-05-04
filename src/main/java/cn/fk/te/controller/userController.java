1bba8065f493cfb7cd59bd13f6938b8261c33ff3
kai.feng <12345678>
2018-05-04 14:48:17 +0800

    111

---
+++ src/main/java/cn/fk/te/controller/userController.java
@@ -0,0 +1,30 @@
+package cn.fk.te.controller;
+
+import cn.fk.te.entity.User;
+import cn.fk.te.service.UserService;
+import cn.fk.te.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class userController {
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @RequestMapping(value = "/insertUser",method = RequestMethod.GET)
+    //@Transactional
+    public String insertUser(){
+
+        User user=new User();
+        user.setUsername("abc");
+        userService.insert(user);
+        Integer aa=4/0;
+        userService.insert(user);
+        return "success";
+    }
+}
