addccc71b4fcc64dadec3589c5ea660cdb9eaaeb
kai.feng <12345678>
2018-07-06 16:55:22 +0800

    改成父子工程

--- te/src/test/java/cn/fk/te/TeApplicationTests.java
+++ te/src/test/java/cn/fk/te/TeApplicationTests.java
@@ -2,6 +2,7 @@ package cn.fk.te;
 
 import cn.fk.te.service.UserService;
 import cn.fk.te.utils.Pair;
+import cn.fk.te.utils.ThreadTaskUtils;
 import cn.fk.te.utils.http.HttpUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,25 +12,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TeApplicationTests {
 
-	@Autowired
-	UserService userService;
+
 
 	@Test
 	public void test123() {
 
-	    Map<String,Object> map= new HashMap<String,Object>();
-	    /*map.put("activityId","2018duanwufuli");
-	    map.put("plateId","main");*/
-	    map.put("mobilePhoneNo","15989494507");
-        Pair<Integer,String> aa= HttpUtil.get("http://localhost:8088/user/testt",map);
 
-
-        System.out.println(aa);
 
 
 
