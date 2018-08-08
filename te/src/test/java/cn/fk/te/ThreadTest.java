af9c4628a0a0354659fc5a2cd94dc30e7ccf7436
fengkai <fengkai@maimaiti.cn>
2018-08-08 14:46:24 +0800

    Revert "0"
    This reverts commit cbc85df

--- te/src/test/java/cn/fk/te/ThreadTest.java
+++ te/src/test/java/cn/fk/te/ThreadTest.java
@@ -18,11 +18,30 @@ public class ThreadTest {
     @Test
     public void tt(){
 
-        System.out.println(0);
-        System.out.println(1);
+        for(int i=0;i<1000;i++){
+            Runnable runnable=new TaskWithoutResult(1000);
+            asynTaskPool.excute(runnable);
+        }
     }
 
 
+    class TaskWithoutResult implements Runnable {
+        private int sleepTime=500;//默认睡眠时间1s
+        public TaskWithoutResult(int sleepTime){
+            this.sleepTime=sleepTime;
+        }
+        @Override
+        public void run(){
+            System.out.println("线程"+Thread.currentThread()+"开始运行");
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {//捕捉中断异常
+
+                System.out.println("线程"+Thread.currentThread()+"被中断");
+            }
+            System.out.println("线程"+Thread.currentThread()+"结束运行");
+        }
+    }
 
 
 }
