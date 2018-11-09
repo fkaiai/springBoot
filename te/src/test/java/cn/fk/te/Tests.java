1d4320788495bc67df6efc25c44c8309d35047e3
fengkai <fengkai@maimaiti.cn>
2018-11-09 16:42:48 +0800

    DateUtil

--- te/src/test/java/cn/fk/te/Tests.java
+++ te/src/test/java/cn/fk/te/Tests.java
@@ -13,6 +13,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static org.assertj.core.util.Hexadecimals.byteToHexString;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Tests {
@@ -23,9 +28,64 @@ public class Tests {
 	@Test
 	public void test123() {
 
+		System.out.println(md51("appid=wxd93613aa9b48b889&noncestr=1J7VOOPLXZ6563DOCVYQW3VG5ZUL2K91&package=Sign=WXPay&partnerid=1401854202&prepayid=wx09161954351867474a113b4e3821290560&timeStamp=1541751594&key=A8B47FA3640EE67CABF779EECB0CEA09"));
+		System.out.println(md52("appid=wxd93613aa9b48b889&noncestr=1J7VOOPLXZ6563DOCVYQW3VG5ZUL2K91&package=Sign=WXPay&partnerid=1401854202&prepayid=wx09161954351867474a113b4e3821290560&timeStamp=1541751594&key=A8B47FA3640EE67CABF779EECB0CEA09"));
 
 
 
+	}
+
+	public static String md51(String str){
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(str.getBytes());
+			byte[] byteDigest = md.digest();
+
+			StringBuffer buf = new StringBuffer("");
+			for (int offset = 0; offset < byteDigest.length; offset++) {
+				int i = byteDigest[offset];
+				if (i < 0)
+					i += 256;
+				if (i < 16)
+					buf.append("0");
+				buf.append(Integer.toHexString(i));
+			}
+
+			return buf.toString();
+		}
+		catch (NoSuchAlgorithmException e){
+
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	public static String md52(String origin){
+		String resultString = null;
+		try {
+			resultString = origin;
+			MessageDigest md = MessageDigest.getInstance("MD5");
+//            md.update(resultString.getBytes("UTF-8"));
+//            resultString = byteArrayToHexString(md.digest());
+			resultString = byteArrayToHexString(md.digest(resultString.getBytes("UTF-8")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultString;
+	}
+
+	/**
+	 * 转换字节数组为16进制字串
+	 * @param b 字节数组
+	 * @return 16进制字串
+	 */
+	public static String byteArrayToHexString(byte[] b) {
+		StringBuilder resultSb = new StringBuilder();
+		for (byte aB : b) {
+			resultSb.append(byteToHexString(aB));
+		}
+		return resultSb.toString();
 	}
 
 
