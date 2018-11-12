7e1878db7287e319189d8bc7e096c9759cd3f0b0
gyl <22400056@qq.com>
2018-11-12 20:48:46 +0800

    新建admin模块

---
+++ admin/src/main/java/cn/fk/admin/Application.java
@@ -0,0 +1,23 @@
+package cn.fk.te;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement(proxyTargetClass = true)
+@MapperScan("cn.fk.te.mapper")
+public class Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+}
